066529188567204b62dce1492f5d1b4d0d8f0727
innfi <innfi.world@gmail.com>
2020-01-29 21:24:05 +0900

    SampleCSharp: initial commit

---
+++ SampleCSharp/SampleDynamoDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+
+namespace SampleCSharp
+{
+    public class SampleDynamoDB
+    {
+        protected readonly AmazonDynamoDBClient client;
+
+        public SampleDynamoDB()
+        {
+            var serviceUrl = "http://127.0.0.1:8000";
+
+            var ddbConfig = new AmazonDynamoDBConfig
+            {
+                ServiceURL = serviceUrl
+            };
+
+            client = new AmazonDynamoDBClient(ddbConfig);
+        }
+    }
+}
