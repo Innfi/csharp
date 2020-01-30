fcc71b81b9aa6ab3b84b7507856277a0a44cfa67
innfi <innfi.world@gmail.com>
2020-01-30 21:39:57 +0900

    CreateTable

---
+++ SampleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SampleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc80c319-a47c-4388-b58c-949f173b39e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SampleTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+
+namespace SampleTest
+{
+    [TestClass]
+    public class SampleTestDynamoDB
+    {
+        [TestMethod]
+        public void Test0CallClient() {
+            var serviceUrl = "http://127.0.0.1:8000";
+            var ddbConfig = new AmazonDynamoDBConfig {
+                ServiceURL = serviceUrl
+            };
+            var client = new AmazonDynamoDBClient(ddbConfig);
+        }
+
+        [TestMethod]
+        public void Test1CreateTable() {
+            var serviceUrl = "http://127.0.0.1:8000";
+            var ddbConfig = new AmazonDynamoDBConfig {
+                ServiceURL = serviceUrl
+            };
+            var client = new AmazonDynamoDBClient(ddbConfig);
+
+            var request = new CreateTableRequest("DummyTable",
+                new List<KeySchemaElement> {
+                    new KeySchemaElement("UserId", KeyType.HASH)
+                },
+                new List<AttributeDefinition> {
+                    new AttributeDefinition("UserId", ScalarAttributeType.N)
+                },
+                new ProvisionedThroughput(1, 1));
+
+            var response = client.CreateTable(request);
+
+            Assert.AreEqual(response.HttpStatusCode, HttpStatusCode.OK);
+        }
+    }
+}
