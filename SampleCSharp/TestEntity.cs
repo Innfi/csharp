1faba44020c830c7917237f8def2cd90ac82eb84
innfi <innfi.world@gmail.com>
2020-02-27 16:55:47 +0900

    entity framework: initial commit

---
+++ SampleCSharp/TestEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+
+namespace SampleCSharp
+{
+    public class TestEntity
+    {
+        public int UserId { get; set; }
+        public int Rank { get; set; }
+        public string Nickname { get; set; }
+    }
+}
---
+++ SampleTest/UnitTestEntityFramework.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleTest
+{
+    public class EntityUserContext : DbContext {
+        public EntityUserContext() : base() { }
+
+        public DbSet<EntityUser> EntityUsers { get; set; }
+    }
+
+    [Table ("EntityUser")]
+    public class EntityUser {
+        public int UserId { get; set; }
+        public int Rank { get; set; }
+        public string Nickname { get; set; }
+    }
+
+    [TestClass]
+    public class UnitTestEntityFramework {
+        [TestMethod]
+        public void Test0InitEntityModel() {
+            var entityContext = new EntityUserContext();
+        }
+
+        [TestMethod]
+        public void Test1AccessDatabaseWithModel() { }
+    }
+}
