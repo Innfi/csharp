996bf289461409e4c4bfab6343821575174a8a95
innfi <innfi.world@gmail.com>
2020-03-09 04:42:01 +0900

    test code refactoring

--- SampleTest/UnitTestJson.cs
+++ SampleTest/UnitTestJson.cs
@@ -7,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SampleTest
 {
-    public class DummyUserData {
+    public class UserData {
         public string UserName;
         public int UserId;
         public string Region;
@@ -16,15 +16,32 @@ namespace SampleTest
     [TestClass]
     public class UnitTestJson {
         [TestMethod]
-        public void Test0CallJson() {
-            var userData = new DummyUserData {
-                UserName = "innfi",
+        public void Test1JsonSerialize() {
+            var userData = new UserData
+            {
+                UserName = "Innfi",
                 UserId = 1,
                 Region = "ap-northeast-2"
             };
 
             var serialized = JsonConvert.SerializeObject(userData, Formatting.Indented);
+            var deserialized = JsonConvert.DeserializeObject<UserData>(serialized);
 
+            Assert.AreEqual(userData.UserName, deserialized.UserName);
+            Assert.AreEqual(userData.UserId, deserialized.UserId);
+            Assert.AreEqual(userData.Region, deserialized.Region);
+        }
+
+
+        [TestMethod]
+        public void Test2JsonStringWriter() {
+            var userData = new UserData {
+                UserName = "Innfi",
+                UserId = 1,
+                Region = "ap-northeast-2"
+            };
+
+            var serialized = JsonConvert.SerializeObject(userData, Formatting.Indented);
             var jsonText = ToDummyJsonText();
 
             Assert.AreEqual(serialized, jsonText);
@@ -32,7 +49,7 @@ namespace SampleTest
 
         protected string ToDummyJsonText() {
             StringBuilder sb = new StringBuilder();
-            StringWriter sw = new StringWriter();
+            StringWriter sw = new StringWriter(sb);
 
             using (var writer = new JsonTextWriter(sw))
             {
@@ -45,8 +62,7 @@ namespace SampleTest
                 writer.WriteValue(1);
                 writer.WritePropertyName("Region");
                 writer.WriteValue("ap-northeast-2");
-
-                writer.WriteEnd();
+                
                 writer.WriteEndObject();
             }
 
