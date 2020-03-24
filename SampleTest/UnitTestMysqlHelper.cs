97c1aec98c8ef69f01b5d60cad5944c19563040c
innfi <innfi.world@gmail.com>
2020-03-24 23:22:56 +0900

    MysqlHelper: initial commit

---
+++ SampleTest/UnitTestMysqlHelper.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SampleTest
+{
+    [TestClass]
+    public class UnitTestMysqlHelper {
+        [TestMethod]
+        public void Test0CallMysqlHelper() {
+
+            var mysqlAddr = "127.0.0.1";
+            var uid = "innfi";
+            var password = "test_password";
+            var database = "test_db";
+            var connectionData = new MySqlConnection {
+                ConnectionString = $"server={mysqlAddr}; uid={uid}; pwd={password}; " +
+                $"database={database}; Allow User Variables=True"
+            };
+
+            var result = MySqlHelper.ExecuteReader(connectionData, "select count(*) from players; ");
+
+            Assert.AreEqual(result.HasRows, true);
+        }
+    }
+}
