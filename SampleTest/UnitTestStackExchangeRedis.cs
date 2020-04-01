0735ca249bfec9bd4c0d11b39a3e685264b9e8ac
innfi <innfi.world@gmail.com>
2020-04-01 21:41:52 +0900

    stackexchange.redis sample

---
+++ SampleTest/UnitTestStackExchangeRedis.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis;
+
+
+namespace SampleTest
+{
+    [TestClass]
+    public class UnitTestStackExchangeRedis {
+        [TestMethod]
+        public void Test0InitRedisConnector() {
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1");
+
+            Assert.AreEqual(redis.IsConnected, true);
+        }
+
+        [TestMethod]
+        public void Test1AccessScalarValue() {
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1");
+            var db = redis.GetDatabase();
+
+            var keyName = "hello";
+            string stringValue = "world";
+
+            db.StringSet(keyName, stringValue);
+            string result = db.StringGet(stringValue);
+
+            Assert.AreEqual(stringValue, result);
+        }
+
+        [TestMethod]
+        public void Test2AccessHashSet() {
+
+        }
+    }
+}
